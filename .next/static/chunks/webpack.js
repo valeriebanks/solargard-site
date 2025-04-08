87ff95bcd20eec7678fa8cdb9c265087923d2db6
valeriebanks <valerie@solargardireland.com>
2025-04-08 13:51:29 +0100

    fix: rename to lowercase folder for Netlify compatibility

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7eb497a9d54127b8"; }
+/******/ 		__webpack_require__.h = function() { return "9db88f69ce11808d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
